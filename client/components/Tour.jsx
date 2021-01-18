9fbe1d49d36b9f13ce1fad7bfee6df231dbbaab6
Matt Carlos <catmatlos@icloud.com>
2021-01-18 15:26:35 -0800

    finished dates section

--- client/components/Tour.jsx
+++ client/components/Tour.jsx
@@ -6,8 +6,49 @@ class Tour extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tourType: 'inperson'
+      tourType: 'inperson',
+      date: '',
+      currentWeek: []
     }
+    this.getCurrentWeek = this.getCurrentWeek.bind(this);
+    this.selectDate = this.selectDate.bind(this)
+  }
+
+  componentDidMount() {
+    var thisWeek = this.getCurrentWeek();
+    this.setState({
+      currentWeek: thisWeek
+    })
+  }
+
+  getCurrentWeek() {
+    let curr = new Date
+    let week = []
+
+    for (let i = 1; i <= 7; i++) {
+      let first = curr.getDate() - curr.getDay() + i
+      let day = new Date(curr.setDate(first)).toISOString().slice(0, 10)
+      week.push(day)
+    }
+
+    for (var day in week ) {
+      week[day] = new Date(week[day].split('-').join(' ')).toString();
+      week[day] = week[day].split(' ')
+      let newFormat = [];
+      newFormat.push(week[day][0])
+      newFormat.push(week[day][1])
+      newFormat.push(week[day][2])
+      week[day] = newFormat;
+    }
+    return week;
+  }
+
+
+
+  selectDate(date) {
+    this.setState({
+      date: date
+    })
   }
 
 
@@ -19,6 +60,11 @@ class Tour extends React.Component {
   }
 
   render() {
+    let props = {
+      currentWeek: this.state.currentWeek,
+      date: this.state.date,
+      selectDate: this.selectDate.bind(this)
+    }
     return(
       <div className="container-tour" id="tour">
 
@@ -27,9 +73,14 @@ class Tour extends React.Component {
           <button onClick={() => {this.handleTourType('inperson')}} className={this.state.tourType === 'inperson' ? "inperson type-active" : "inperson"}>In-Person</button>
           <button onClick={() => {this.handleTourType('video')}} className={this.state.tourType === "video" ? "videochat type-active" : "videochat"}>Video Chat</button>
         </div>
-
-          <Dates />
-
+          <Dates props={props}/>
+        <div>
+          <form>
+            <select>
+              <option>time</option>
+            </select>
+          </form>
+        </div>
       </div>
     )
   }
