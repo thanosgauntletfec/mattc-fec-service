b143afed4463bb8f3f21f92043d927f8cccb0e64
Matt Carlos <catmatlos@icloud.com>
2021-01-28 10:26:16 -0800

    changed port to process.env

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const db = require('../db');
 const cors = require('cors')
 
-const port = 2080;
+const port = provess.env.PORT || 2080;
 
 app.use(cors({
   origin: 'http://localhost:3080'
